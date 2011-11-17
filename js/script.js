0a02bea29b2114cd775338b3aa101a0bbbbb3067
Feross Aboukhadijeh <feross@gmail.com>
2011-11-17 01:30:49 -0800

    Lots of site improvements (see list below) - Made events look a lot cleaner - Hide subscribe box by default - Use semantic <time> tag instead of h2 for event dates - Improved how events get marked as old. Now, you specify a year for each quarter using a data-year attribute. - Improved how media-queries handle the nav links - Copied all of fall quarter's event to the Past Events page

--- js/script.js
+++ js/script.js
@@ -8,19 +8,23 @@ $(function() {
 });
 
 function addSubscribeHandler() {
-  $('.subscribe').submit(function(e) {
-    $('.subscribe [type="submit"]').attr('disabled', 'disabled');
+  $('.subscribe').click(function(e) {
+    $('.subscribeBox').slideDown();
+  });
+
+  $('.subscribeBox').submit(function(e) {
+    $('.subscribeBox [type="submit"]').attr('disabled', 'disabled');
 
     var url = 'http://cgi.stanford.edu/group/acm/cgi-bin/email_signup_all.php';
 
     $.ajax(url, {
       dataType: "jsonp",
       data: {
-        email: $('.subscribe [name="email"]').val()
+        email: $('.subscribeBox [name="email"]').val()
       }
     }).success(function(e) {
-      $('.subscribe').slideUp(function() {
-        $('.subscribe')
+      $('.subscribeBox').slideUp(function() {
+        $('.subscribeBox')
           .empty()
           .html('<h2 style="color: green;">Success! Click the link in your email to confirm.</h2>')
           .slideDown();
@@ -29,7 +33,7 @@ function addSubscribeHandler() {
     }).error(function(e) {
       alert('There was an error and you were NOT subscribed to the mailing list. If you\'re feeling kind, send an email to the ACM officers list (acm-officers@lists.stanford.edu) or Feross (feross@feross.org) and let us know so we can fix it.');
 
-      $('.subscribe [type="submit"]')
+      $('.subscribeBox [type="submit"]')
         .removeAttr('disabled');
     });
 
@@ -45,41 +49,60 @@ function addSubscribeHandler() {
  */
 function addDateHighlighter(selector, pastType) {
   var DAY = 1000 * 60 * 60 * 24;
-  var currentYear = (new Date(Date.now())).getFullYear();
-
   var isPast = false;
   var numOld = 0;
-  var seenH1 = false;
+  var currentYear;
+
+  var helper = function() {
+    if (numOld > 0) {
+      var $oldMsg = $('<span class="oldMsg">Show <strong>'+numOld+'</strong> past '+pastType+' this quarter</span>');
+      var $quarterEvents = $(selector).find('[data-year='+currentYear+']');
+
+      $oldMsg.click(function(e) {
+        $quarterEvents.find('.oldMsg').remove();
+        $quarterEvents.nextAll().each(function(i, e) {
+          var $e = $(e);
+          if ($e.is('h2')) {
+            return false; // stop execution
+          }
+          if ($e.is('.old')) {
+            $e.removeClass('old');
+            $e.fadeIn();
+          }
+        });
+      });
+      $quarterEvents.append($oldMsg);
+    }
+  }
+
   $(selector).children().each(function(i, e) {
     var $e = $(e);
-    if ($e.is('h1')) {
-        if (seenH1) {
-            return false; // we're looking at future quarter events; always show those.
-        }
-        seenH1 = true;
-    }
-    if ($e.is('h2')) {
+    if ($e.is('h2')) { // start of a new quarter's events
+      helper();
+      currentYear = $e.data('year');
+      numOld = 0;
       isPast = false;
-      var date = Date.parse($e.text() + ', ' + currentYear);
+    }
+    if ($e.is('time')) {
+      isPast = false;
+      var date;
+      if ($e.text().indexOf(',') > 0) {
+        date = Date.parse($e.text()); // date in <time> already contains the year
+      } else {
+        date = Date.parse($e.text() + ', ' + currentYear); // add in the year
+      }
       if (date < Date.now() - DAY) {
-        $e.addClass('old'); // mark DATE as old
+        $e.addClass('old').hide(); // mark DATE as old
         isPast = true;
       }
     } else {
       if (isPast) {
-        $e.addClass('old'); // mark EVENT as old
+        $e.addClass('old').hide(); // mark EVENT as old
         numOld += 1;
       }
     }
   });
 
-  if (numOld > 0) {
-      var $oldMsg = $('<div class="oldMsg">' + numOld + ' past ' + pastType + ' this quarter</div>');
-      $oldMsg.click(function(e) {
-          $(selector).find('.old').fadeIn();
-          $(selector).find('.oldMsg').remove();
-      });
-      $(selector).find('h1').first().after($oldMsg);
-  }
+  helper();
 
 }
