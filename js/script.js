245418b7fa9c20f25a235b134631d8fa352eb208
Richard Chen <richardchen39@gmail.com>
2016-11-16 14:40:18 -0800

    Add new pages

--- js/script.js
+++ js/script.js
@@ -11,18 +11,21 @@
     });
 
     var path = window.location.pathname;
-    if (path === '/about/') {
-      $('.sidebar-left .links a[name="about"]').toggleClass('selected');
-    } else if (path === '/' || path === '/index.html') {
+    if (path === '/' || path === '/index.html') {
       $('.sidebar-left .links a[name="home"]').toggleClass('selected');
+    } else if (path === '/about/') {
+      $('.sidebar-left .links a[name="about"]').toggleClass('selected');
+    } else if (path === '/contact/') {
+      $('.sidebar-left .links a[name="contact"]').toggleClass('selected');
+    } else if (path === '/officers/') {
+      $('.sidebar-left .links a[name="officers"]').toggleClass('selected');
     } else if (path === '/sponsors/') {
       $('.sidebar-left .links a[name="sponsors"]').toggleClass('selected');
     }
   });
-  
+
   $(document).mousemove(function(e) {
     $('svg.gear').css('transform', 'rotate('+(((e.pageX/5)%360)+((e.pageY/5)%360))+'deg)');
   });
 
 })(this, this.document);
-  
